607fff9f719817617af48a7148e4e6cfbf779c17
AlexandruOlteanu <alexandruolteanu2001@gmail.com>
2023-10-09 22:58:49 +0300

    Added api, client, service

---
+++ src/main/java/com/retrox/client/RetroXPublicService.java
@@ -0,0 +1,6 @@
+package com.retrox.client;
+
+import com.retrox.api.domain.RetroXService;
+
+public interface RetroXPublicService extends RetroXService {
+}
---
+++ src/main/java/com/retrox/service/web/RetroXController.java
@@ -0,0 +1,11 @@
+package com.retrox.service.web;
+
+import com.retrox.api.domain.RetroXService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RetroXController implements RetroXService {
+
+
+
+}
