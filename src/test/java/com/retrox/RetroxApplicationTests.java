152081d5f0fee90cf663ab9d422acb8f66a51f5d
AlexandruOlteanu <alexandruolteanu2001@gmail.com>
2023-10-09 22:45:45 +0300

    Starting point

---
+++ src/test/java/com/retrox/RetroxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retrox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetroxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
